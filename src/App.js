dd2631d595d1cd10b5adff525da11a4fed4f2504
Y.M <66901428+yuichimukai@users.noreply.github.com>
2021-05-30 14:35:31 +0900

    react oprtimaization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -10,13 +10,15 @@ export default function App() {
 
   const onClickOpen = () => setOpen(!open);
 
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
